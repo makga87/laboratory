055d8d17a0fa61dad61ab7a82acf96ea1640e100
hgkim <makga87@naver.com>
2022-01-23 23:58:07 +0900

    [RestDoc] docker에 restdoc 심기

---
+++ spring-boot-docker/src/test/java/com/example/docker/controller/HelloWorldControllerTest.java
@@ -0,0 +1,44 @@
+package com.example.docker.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HelloWorldController.class)
+@AutoConfigureRestDocs(outputDir = "target/restdoc/snippets")
+class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private HelloWorldController helloWorldController;
+
+    @Test
+    public void helloWorldTest() throws Exception {
+
+        Mockito.when(helloWorldController.helloWorld())
+                .thenReturn(ResponseEntity.ok("Hello, World"));
+
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")))
+                .andDo(document("helloWorld"));
+    }
+}
