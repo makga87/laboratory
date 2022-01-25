fe227bf9719de20d425b4005229770ed9d5f2df8
hgkim <makga87@naver.com>
2022-01-26 01:11:59 +0900

    [RestDocApiSpec] 구성중이나, Json이 잘 안뽑힘

--- spring-boot-docker/src/main/java/com/example/docker/controller/HelloWorldController.java
+++ spring-boot-docker/src/main/java/com/example/docker/controller/HelloWorldController.java
@@ -4,11 +4,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloWorldController {
 
     @RequestMapping("/")
-    public ResponseEntity<String> helloWorld(){
-        return ResponseEntity.ok("Hello World~");
+    public ResponseEntity<Map> helloWorld(){
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("data", "Hello World");
+        return ResponseEntity.ok(dataMap);
     }
 }
--- spring-boot-docker/src/test/java/com/example/docker/controller/HelloWorldControllerTest.java
+++ spring-boot-docker/src/test/java/com/example/docker/controller/HelloWorldControllerTest.java
@@ -8,12 +8,18 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.resourceDetails;
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,13 +38,22 @@ class HelloWorldControllerTest {
     @Test
     public void helloWorldTest() throws Exception {
 
-        Mockito.when(helloWorldController.helloWorld())
-                .thenReturn(ResponseEntity.ok("Hello, World"));
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("data", "Hello World");
 
-        this.mockMvc.perform(get("/"))
+        Mockito.when(helloWorldController.helloWorld())
+                .thenReturn(ResponseEntity.ok(resultMap));
+
+        this.mockMvc.perform(RestDocumentationRequestBuilders.get("/"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, World")))
-                .andDo(document("helloWorld"));
+                .andDo(document(
+                                "helloWorld",
+                                resourceDetails().description("hello world description"),
+                                responseFields(
+                                        fieldWithPath("data").description("hello world string")
+                                )
+                        )
+                );
     }
 }
