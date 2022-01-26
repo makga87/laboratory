4abf5506096ca0928bd9adc3cd8829baae85ecaf
hyunggeun.kim <makga87@naver.com>
2022-01-26 19:48:35 +0900

    [RestDocs] yaml은 나오나 표시가 안됨

--- spring-boot-docker/start.sh
+++ spring-boot-docker/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker run -d -p 80:8080 \
+docker run -d -p 8012:8080 \
   -e URLS_PRIMARY_NAME=SpringRestdocs \
   -e URLS="[ \
     { url: 'docs/swagger.json', name: 'Swagger' } \2
     , { url: 'docs/restdocs.json', name: 'SpringRestdocs' } \
   ]" \
-  -v /Users/hgkim/Desktop/intellij/workspace/laboratory/spring-boot-docker/target/restdoc/snippets/helloWorld/:/usr/share/nginx/html/docs/ \
+  -v "$PWD"/target/restdoc-spec/:/usr/share/nginx/html/docs/ \
   swaggerapi/swagger-ui
