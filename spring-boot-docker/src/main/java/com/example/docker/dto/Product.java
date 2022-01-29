78c796a403b5d64f1564bfac929b1dc6c27c4e97
hgkim <makga87@naver.com>
2022-01-29 23:52:36 +0900

    [ELKStack] docker 설정 중

---
+++ spring-boot-docker/src/main/java/com/example/docker/dto/Product.java
@@ -0,0 +1,17 @@
+package com.example.docker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Product {
+
+    private int id;
+    private String name;
+    private String type;
+}
