0c3a690491cdf6ba6bf9c98648bb72ccb6394aa0
hgkim <makga87@naver.com>
2022-01-30 23:40:57 +0900

    [ELKStack] logstash 설정중

--- spring-boot-docker/logstash/start.sh
+++ spring-boot-docker/logstash/start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 docker run --rm -it \
+   -p 15044:15044 \
    -v $PWD/pipeline/:/usr/share/logstash/pipeline \
-   -v $PWD/config/:/usr/share/logstash/config \
+   -v $PWD/config/logstash.yml:/usr/share/logstash/config/logstash.yml \
+   -v $PWD/config/pipelines.yml:/usr/share/logstash/config/pipelines.yml \
    docker.elastic.co/logstash/logstash:7.16.3
