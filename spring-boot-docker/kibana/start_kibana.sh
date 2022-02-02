426b6fc715a2569d3fcc75164da719352ba4ffac
hgkim <makga87@naver.com>
2022-02-02 23:31:05 +0900

    [ELKStack] 컨테이너가 2개까지만 뜨는 현상

--- spring-boot-docker/kibana/start_kibana.sh
+++ spring-boot-docker/kibana/start_kibana.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-docker run -p 127.0.0.1:5601:5601 \
+docker run --rm -it -d \
+  -p 5601:5601 \
   -e "ELASTICSEARCH_HOSTS=http://host.docker.internal:9200" \
-  docker.elastic.co/kibana/kibana:7.17.0
+  docker.elastic.co/kibana/kibana:7.16.3
