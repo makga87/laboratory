86f93a68a91276d238eee654c0b61fe849799771
hgkim <makga87@naver.com>
2022-02-03 23:02:56 +0900

    [ELKStack] ELK 설정 완료

--- spring-boot-docker/elasticsearch/start_elastic.sh
+++ spring-boot-docker/elasticsearch/start_elastic.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-docker run --rm -it -d \
+docker run --rm -it \
   -p 9200:9200 \
   -p 9300:9300 \
   -e "discovery.type=single-node" \
+  -e "ES_JAVA_OPTS=-Xmx256m -Xms256m" \
   docker.elastic.co/elasticsearch/elasticsearch:7.16.3
--- spring-boot-docker/logstash/start_logstash.sh
+++ spring-boot-docker/logstash/start_logstash.sh
@@ -3,6 +3,7 @@
 docker run --rm -it \
    -p 15044:15044 \
    -p 9600:9600 \
+   -e "LS_JAVA_OPTS=-Xmx256m -Xms256m" \
    -v $PWD/pipeline/:/usr/share/logstash/pipeline \
    -v $PWD/config/logstash.yml:/usr/share/logstash/config/logstash.yml \
    -v $PWD/config/pipelines.yml:/usr/share/logstash/config/pipelines.yml \
