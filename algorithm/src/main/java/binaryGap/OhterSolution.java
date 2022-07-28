ef7d6f9757512ffdae27dbaf1cc2b2bfa7722bc8
Kim HyungGeun <hyunggeun.kim@nhnsoft.com>
2022-07-28 14:19:30 +0900

    바이너리 갭 다른 솔루션

---
+++ algorithm/src/main/java/binaryGap/OhterSolution.java
@@ -0,0 +1,21 @@
+package binaryGap;
+
+public class OhterSolution {
+
+    public int solution(int N) {
+
+        int result = 0;
+        int count = 0;
+        String str = Integer.toString(N , 2);
+        for(int i = 1; i < str.length(); i++) {
+            if(str.charAt(i) == '0') {
+                count++;
+            }else {
+                result = Math.max(result, count);
+                count = 0;
+            }
+        }
+        return result;
+    }
+}
+
