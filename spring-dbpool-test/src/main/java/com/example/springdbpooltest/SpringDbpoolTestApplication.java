2ddaa3d526e05b98d1b4e4c6ae2289ec4db406d9
Kim HyungGeun <makga87@naver.com>
2022-08-16 23:25:52 +0900

    # initial commit

---
+++ spring-dbpool-test/src/main/java/com/example/springdbpooltest/SpringDbpoolTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdbpooltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbpoolTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbpoolTestApplication.class, args);
+    }
+
+}
