1d9986cb4c5601b6ac36fb70afced4eae84e536b
김형근 <makga87@naver.com>
2022-09-23 00:13:21 +0900

    # 코딜리티 - 소팅

---
+++ algorithm/src/main/java/codility/lessons/sort/triangle/Solution.java
@@ -0,0 +1,22 @@
+package codility.lessons.sort.triangle;
+
+import java.util.Arrays;
+
+/**
+ *
+ */
+public class Solution {
+
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int result = 0;
+
+        Arrays.sort(A);
+
+        for(int i = A.length - 1; i >= 2; i--) {
+            if(A[i] - A[i-1] < A[i - 2]) return 1;
+        }
+
+        return result;
+    }
+}
