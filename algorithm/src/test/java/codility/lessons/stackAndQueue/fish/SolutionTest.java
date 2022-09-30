971328dd9563f3832f0dd8d514ca9d9471e666a8
Kim HyungGeun <makga87@naver.com>
2022-09-30 16:01:13 +0900

    # 코딜리티 소스 - 스택 큐

---
+++ algorithm/src/test/java/codility/lessons/stackAndQueue/fish/SolutionTest.java
@@ -0,0 +1,17 @@
+package codility.lessons.stackAndQueue.fish;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @ParameterizedTest
+    @ValueSource()
+    void testSolution(){
+
+    }
+
+}
