a0d55c925bb9d410a7de59988be3d373d4e57ab1
김형근 <makga87@naver.com>
2022-10-12 23:11:19 +0900

    # 코딜리티 - 리더 - 도미네이터

--- algorithm/src/main/java/codility/lessons/leader/dominator/Solution.java
+++ algorithm/src/main/java/codility/lessons/leader/dominator/Solution.java
@@ -8,28 +8,24 @@ import java.util.HashMap;
 class Solution {
     public int solution(int[] A) {
         // write your code in Java SE 8
-        if(A.length == 0) return -1;
-        if(A.length == 1 || A.length == 2) return 0;
+        int length = A.length;
+        if(length == 0) return -1;
+        if(length == 1 || length == 2) return 0;
 
-        int dominatorSize = A.length % 2 == 0 ? A.length / 2 : (A.length / 2) + 1;
+        int moreThan = (length/2) + 1;
 
         HashMap<Integer, Integer> dataMap = new HashMap();
-
-        for(int i = 0; i < A.length; i++) {
-
+        for(int i = 0; i < length; i++) {
             if(dataMap.containsKey(A[i])) {
-
-                int value = dataMap.get(A[i]);
-                value++;
-                if(value >= dominatorSize) return i;
-                else {
-                    dataMap.put(A[i], value);
-                }
-
+                int cnt = dataMap.get(A[i]) + 1;
+                if(cnt >= moreThan) return i;
+                dataMap.put(A[i], cnt);
             } else {
                 dataMap.put(A[i], 1);
+
             }
         }
+
         return -1;
     }
 }
