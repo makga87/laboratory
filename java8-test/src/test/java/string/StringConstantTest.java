0ab3865c3b22599dea72cc918d3e2ef3fc0defd1
Kim HyungGeun <makga87@naver.com>
2023-03-09 18:45:52 +0900

    문자열 리터럴 성능 테스트

--- java8-test/src/test/java/string/StringConstantTest.java
+++ java8-test/src/test/java/string/StringConstantTest.java
@@ -9,14 +9,15 @@ class StringConstantTest {
 
 	private static String EMOJI_BLUSH = "\uD83D\uDE0A";
 
-//	private static String testStr = "관람일이 바로 내일이에요!\n 빠르고 간편하게 입장할 수 있도록 지금 얼굴 입장을 설정해 주세요 \uD83D\uDE0A";
+//	private static String testStr = "상품이 출시되었습니다.!\n 구매하러 와주세요 \uD83D\uDE0A";
+
 	@Test
 	void 상수와_문자열_조합_성능_비교(){
 
 		long start = System.currentTimeMillis();
 		for (int i = 0; i < 1000000000; i++) {
-			String s = "관람일이 바로 내일이에요!\n"
-			+ "빠르고 간편하게 입장할 수 있도록 지금 얼굴 입장을 설정해 주세요"+EMOJI_BLUSH;
+			String s = "상품이 출시되었습니다.!\n "
+			+ "구매하러 와주세요 "+EMOJI_BLUSH;
 
 //			String s = testStr;
 		}
