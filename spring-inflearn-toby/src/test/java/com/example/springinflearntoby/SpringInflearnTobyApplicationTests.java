ddf5ee8eab042e7d6f1538ea9118ab1a029c6c15
hgkim87 <hgkim8704@gmail.com>
2023-04-04 00:02:14 +0900

    [ADD] 인프런 토비 스프링 공부

---
+++ spring-inflearn-toby/src/test/java/com/example/springinflearntoby/SpringInflearnTobyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinflearntoby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInflearnTobyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
