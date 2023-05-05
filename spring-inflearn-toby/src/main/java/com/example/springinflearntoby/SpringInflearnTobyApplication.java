6cc992b9579aed6599151aed0d7c78a33391da6e
hgkim87 <hgkim8704@gmail.com>
2023-05-05 17:58:50 +0900

    [ADD] 팩터리와 어노테이션 방식으로 빈주입, 어플리케이션컨텍스트 생성 방식

--- spring-inflearn-toby/src/main/java/com/example/springinflearntoby/SpringInflearnTobyApplication.java
+++ spring-inflearn-toby/src/main/java/com/example/springinflearntoby/SpringInflearnTobyApplication.java
@@ -1,16 +1,20 @@
 package com.example.springinflearntoby;
 
 import com.example.springinflearntoby.controller.HelloController;
+import com.example.springinflearntoby.controller.HelloService;
 import com.example.springinflearntoby.controller.SimpleHelloService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -20,12 +24,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@SpringBootApplication
+@Configuration
 public class SpringInflearnTobyApplication {
+
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
 
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh(); // NOTE : 절대 생략 하면 안되
@@ -39,8 +53,7 @@ public class SpringInflearnTobyApplication {
                 webServer.start();
             }
         };
-        applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.registerBean(HelloController.class);
+        applicationContext.register(SpringInflearnTobyApplication.class);
         applicationContext.refresh();
 
 
