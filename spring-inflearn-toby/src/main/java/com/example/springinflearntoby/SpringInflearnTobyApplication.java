294bb3084d6c36e2301e9133e584ce591ef1575a
hgkim87 <makga87@naver.com>
2023-05-10 23:07:16 +0900

    [ADD] 팩터리와 어노테이션 방식으로 빈주입, 어플리케이션컨텍스트 생성 방식

--- spring-inflearn-toby/src/main/java/com/example/springinflearntoby/SpringInflearnTobyApplication.java
+++ spring-inflearn-toby/src/main/java/com/example/springinflearntoby/SpringInflearnTobyApplication.java
@@ -1,62 +1,29 @@
 package com.example.springinflearntoby;
 
-import com.example.springinflearntoby.controller.HelloController;
-import com.example.springinflearntoby.controller.HelloService;
-import com.example.springinflearntoby.controller.SimpleHelloService;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 @Configuration
+@ComponentScan
 public class SpringInflearnTobyApplication {
 
     @Bean
-    public HelloController helloController(HelloService helloService) {
-        return new HelloController(helloService);
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
 
     @Bean
-    public HelloService helloService() {
-        return new SimpleHelloService();
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
     }
 
     public static void main(String[] args) {
-
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh(); // NOTE : 절대 생략 하면 안되
-                ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
-                WebServer webServer = servletWebServerFactory.getWebServer((servletContext) -> {
-
-                    servletContext
-                            .addServlet("dispacherServlet", new DispatcherServlet(this))
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(SpringInflearnTobyApplication.class);
-        applicationContext.refresh();
-
-
+        MySpringApplication.run(SpringInflearnTobyApplication.class, args);
     }
+
 
 }
--- spring-inflearn-toby/src/main/java/com/example/springinflearntoby/controller/HelloController.java
+++ spring-inflearn-toby/src/main/java/com/example/springinflearntoby/controller/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.springinflearntoby.controller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
+@RestController
 @RequestMapping("/hello")
 public class HelloController {
 
