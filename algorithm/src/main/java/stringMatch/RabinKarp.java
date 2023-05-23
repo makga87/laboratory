95606a351e120b676147409adad99031e11b11e8
hgkim87 <hgkim8704@gmail.com>
2023-05-24 01:43:12 +0900

    [ADD] StringMatch RabinKarp - mine

--- algorithm/src/main/java/stringMatch/RabinKarp.java
+++ algorithm/src/main/java/stringMatch/RabinKarp.java
@@ -1,32 +1,58 @@
 package stringMatch;
 
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 public class RabinKarp {
 
-    private static String INPUT = "cacacacacacat";
+    private static String INPUT = "cacacat";
 
     public static void main(String[] args) {
 
         RabinKarp rabinKarp = new RabinKarp();
 
         String keyWord = "cat";
-        keyWord.toCharArray();
 
-        int keyWordHashCode = rabinKarp.getHashCode(keyWord);
+        char[] keywordSearchArr = new char[keyWord.length()];
 
-        for (Character ch : RabinKarp.INPUT.toCharArray()) {
+        int keyWordHashCode = rabinKarp.getHashCode(keyWord.toCharArray());
+
+        int currIdx = 0;
+
+        while (currIdx <= (INPUT.length() - keyWord.length())) {
+
+            for (int j = 0; j < keyWord.length(); j++) {
+                keywordSearchArr[j] = INPUT.charAt(j + currIdx);
+            }
+            System.out.println(String.valueOf(keywordSearchArr));
+
+            int inputHashCode = rabinKarp.getHashCode(keywordSearchArr);
+            if (inputHashCode == keyWordHashCode) {
+                if (rabinKarp.check(keyWord.toCharArray(), keywordSearchArr)) {
+                    System.out.println("Found keyword in INPUT. The position start is " + currIdx);
+                }
+            }
+
+            currIdx++;
 
         }
+
     }
 
-    private int getHashCode(String str) {
+    private int getHashCode(char[] chars) {
+
         int sum = 0;
 
-        for (Character ch : str.toCharArray()) {
+        for (Character ch : chars) {
             sum += ch;
         }
+        System.out.printf("Hashcode : %d, chars : %s \n", sum, String.valueOf(chars));
         return sum;
+    }
+
+    private boolean check(char[] keyword, char[] temp) {
+        boolean result = true;
+        for (int i = 0; i < keyword.length; i++) {
+            result &= keyword[i] == temp[i];
+        }
+
+        return result;
     }
 }
