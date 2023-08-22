8a7a4108f126a4b63614a7294ff694df169fdac0
Kim HyungGeun <makga87@naver.com>
2023-08-22 13:38:57 +0900

    [FEAT] 테스트 케이스 추가 (setter 없는 케이스)

--- spring-web-test/src/main/java/com/example/springwebtest/message/conv/model/User5.java
+++ spring-web-test/src/main/java/com/example/springwebtest/message/conv/model/User5.java
@@ -7,30 +7,21 @@ import lombok.extern.slf4j.Slf4j;
 @Data
 public class User5 implements Response {
 
-    private String emailYn;
-    private String marcketingYn;
+	private String emailYn;
+	private String marcketingYn;
 
-    public User5(String emailYn, String marcketingYn) {
-        log.debug("### User5 파라메터 생성자");
-        this.emailYn = emailYn;
-        this.marcketingYn = marcketingYn;
-    }
+	public User5(String emailYn, String marcketingYn) {
+		log.debug("### User5 파라메터 생성자");
+		this.emailYn = emailYn;
+		this.marcketingYn = marcketingYn;
+	}
 
-    public String getEmailYn() {
-        log.debug("### getEmailYn 호출");
-        return getMarcketingYn();
-    }
+	public String getEmailYn() {
+		log.debug("### getEmailYn 호출");
+		return getMarcketingYn();
+	}
 
-    public void setEmailYn(String emailYn) {
-        log.debug("### setEmailYn 호출");
-        setMarcketingYn(emailYn);
-    }
-
-    public String getMarcketingYn() {
-        return marcketingYn;
-    }
-
-    public void setMarcketingYn(String marcketingYn) {
-        this.marcketingYn = marcketingYn;
-    }
+	public String getMarcketingYn() {
+		return marcketingYn;
+	}
 }
