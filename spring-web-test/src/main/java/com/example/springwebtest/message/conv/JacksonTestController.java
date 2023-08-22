ce43c073ed52a4da47f17d8f134687b68b7965ec
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-08-22 11:17:58 +0900

    [FEAT] 테스트 케이스 추가

--- spring-web-test/src/main/java/com/example/springwebtest/message/conv/JacksonTestController.java
+++ spring-web-test/src/main/java/com/example/springwebtest/message/conv/JacksonTestController.java
@@ -4,6 +4,9 @@ import com.example.springwebtest.message.conv.model.Response;
 import com.example.springwebtest.message.conv.model.User1;
 import com.example.springwebtest.message.conv.model.User2;
 import com.example.springwebtest.message.conv.model.User3;
+import com.example.springwebtest.message.conv.model.User4;
+import com.example.springwebtest.message.conv.model.User5;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,20 +18,32 @@ public class JacksonTestController {
 
     @PostMapping("/jackson/test1")
     public Response jacksonTest(@RequestBody User1 user) {
-        log.debug("user1 : {}", user);
+        log.debug("[AllArgs + NoArgus] user1 : {}", user);
         return user;
     }
 
 
     @PostMapping("/jackson/test2")
     public Response jacksonTest(@RequestBody User2 user) {
-        log.debug("user2 : {}", user);
+        log.debug("[NoArgus] user2 : {}", user);
         return user;
     }
 
     @PostMapping("/jackson/test3")
     public Response jacksonTest(@RequestBody User3 user) {
-        log.debug("user3 : {}", user);
+        log.debug("[AllArgs] user3 : {}", user);
         return user;
     }
+
+	@PostMapping("/jackson/test4")
+	public Response jacksonTest(@RequestBody User4 user) {
+		log.debug("[AllArgs + NoArgus(private)] user4 : {}", user);
+		return user;
+	}
+
+	@PostMapping("/jackson/test5")
+	public Response jacksonTest(@RequestBody User5 user) {
+		log.debug("user5 : {}", user);
+		return user;
+	}
 }
