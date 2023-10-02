72e10729e6ab720ad1bfef3172f3bab05503525c
hgkim87 <makga87@naver.com>
2023-10-02 18:34:26 +0900

    [ADD] Custom @Conditional의 추가

--- spring-inflearn-toby/src/main/java/com/example/springinflearntoby/config/autoconfig/JettyWebserverConfig.java
+++ spring-inflearn-toby/src/main/java/com/example/springinflearntoby/config/autoconfig/JettyWebserverConfig.java
@@ -2,16 +2,16 @@ package com.example.springinflearntoby.config.autoconfig;
 
 import com.example.springinflearntoby.config.ConditionalMyOnClass;
 import com.example.springinflearntoby.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
-class TomcatWebserverConfig {
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
+public class JettyWebserverConfig {
 
-    @Bean("tomcatWebServerFactory")
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
