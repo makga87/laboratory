6e977a4088549e0daf01453d8393f08f2fff8f85
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-12-11 19:22:14 +0900

    [ADD] BigDecimalTest

--- java-api-test/src/test/java/bigdecimal/BigDecimalTest.java
+++ java-api-test/src/test/java/bigdecimal/BigDecimalTest.java
@@ -2,6 +2,7 @@ package bigdecimal;
 
 import java.math.BigDecimal;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BigDecimalTest {
@@ -15,15 +16,41 @@ public class BigDecimalTest {
 		// 46.68
 		System.out.println(value1 + value2);
 
-
 		BigDecimal bigDecimal =  BigDecimal.valueOf(12.23);
 		BigDecimal afterAdded = bigDecimal.add(BigDecimal.valueOf(34.45));
 		System.out.println(afterAdded);
 	}
 
 	@Test
-	void NULL값이_들어가는_경우() {
-//		System.out.println(BigDecimal.valueOf("abc"));
+	void 문자열_값이_들어가는_경우_NumberFormatException을_발생시킨다() {
+		System.out.println(new BigDecimal("abc"));
+	}
+
+	@Test
+	void 빈문자열_값이_들어가는_경우_NumberFormatException을_발생시킨다() {
 		System.out.println(new BigDecimal(""));
+	}
+
+	/**
+	 * null은 입력값 불가
+	 */
+	@Test
+	void Null_값이_들어가는_경우_NumberFormatException을_발생시킨다() {
+//		System.out.println(new BigDecimal(null));
+	}
+
+	@Test
+	void 비교(){
+		BigDecimal a = new BigDecimal("1.011");
+		BigDecimal b = new BigDecimal("1.0110");
+
+		// 주소 비교니 당연히 false
+		Assertions.assertFalse(a == b);
+
+		// 소숫점 끝자리까지 전부 동일해야하므로 false
+		Assertions.assertFalse(a.equals(b));
+
+		// 값의 비교시엔, 끝자리 0은 무시됨
+		Assertions.assertTrue(a.compareTo(b) == 0);
 	}
 }
