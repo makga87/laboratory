a5dd13e0c3a0e4c3226dd4c1c8ac0714344d9a99
hgkim87 <hgkim8704@gmail.com>
2024-01-11 21:04:17 +0900

    [ADD] Spring Redis 연동시 발생하는 내용들 테스트를 위한 리포지토리

---
+++ spring-redis-test/src/main/java/com/example/springredistest/SpringRedisTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springredistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisTestApplication.class, args);
+    }
+
+}
