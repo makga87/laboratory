8e0c5cb3d753e1d8eb05b4cc8021872bfbe44802
Kim HyungGeun <hyunggeun.kim@bugs.com>
2024-03-19 16:13:39 +0900

    [FIX] Redis List, Set, Sorted Set 테스트

--- spring-redis-test/src/test/java/com/example/springredistest/config/RedisConfigurationTest.java
+++ spring-redis-test/src/test/java/com/example/springredistest/config/RedisConfigurationTest.java
@@ -48,4 +48,76 @@ class RedisConfigurationTest {
 	}
 
 
+	@DisplayName("Redis List 타입 데이터 테스트")
+	@Test
+	void testRedisListDataType() {
+
+
+		String key = "tickets";
+		redisTemplate.opsForList().getOperations().delete(key);
+
+		redisTemplate.opsForList().rightPush(key, new Ticket(1, 1));
+		redisTemplate.opsForList().leftPush(key, new Ticket(2, 2));
+		redisTemplate.opsForList().rightPush(key, new Ticket(3, 3));
+		System.out.println("==================================================");
+		System.out.println(redisTemplate.opsForList().getOperations().opsForList().range(key, 0, -1));
+		System.out.println("==================================================");
+
+		redisTemplate.opsForList().rightPop(key);
+		redisTemplate.opsForList().rightPop(key);
+
+		System.out.println("==================================================");
+		System.out.println(redisTemplate.opsForList().getOperations().opsForList().range(key, 0, -1));
+		System.out.println("==================================================");
+		redisTemplate.opsForList().getOperations().delete(key);
+
+	}
+
+
+	@DisplayName("Redis Set 타입 데이터 테스트")
+	@Test
+	void testRedisSetDataType() {
+
+		String key = "tickets";
+		redisTemplate.opsForSet().getOperations().delete(key);
+
+		redisTemplate.opsForSet().add(key, new Ticket(1, 1));
+		redisTemplate.opsForSet().add(key, new Ticket(2, 2));
+		redisTemplate.opsForSet().add(key, new Ticket(3, 3));
+		redisTemplate.opsForSet().add(key, new Ticket(4, 4));
+		redisTemplate.opsForSet().add(key, new Ticket(5, 5));
+		System.out.println(redisTemplate.opsForSet().members(key));
+		System.out.println("==================================================");
+
+		redisTemplate.opsForSet().pop(key);
+
+		System.out.println("==================================================");
+		System.out.println(redisTemplate.opsForSet().members(key));
+		System.out.println("==================================================");
+
+	}
+
+	@DisplayName("Redis Sorted Set 타입 데이터 테스트")
+	@Test
+	void testRedisSortedSetDataType() {
+
+		String key = "tickets";
+		redisTemplate.opsForZSet().getOperations().delete(key);
+
+		redisTemplate.opsForZSet().add(key, new Ticket(1, 1), 5);
+		redisTemplate.opsForZSet().add(key, new Ticket(2, 2), 4);
+		redisTemplate.opsForZSet().add(key, new Ticket(3, 3), 3);
+		redisTemplate.opsForZSet().add(key, new Ticket(4, 4), 2);
+		redisTemplate.opsForZSet().add(key, new Ticket(5, 5), 1);
+		System.out.println(redisTemplate.opsForZSet().range(key, 0, -1));
+		System.out.println("==================================================");
+
+		redisTemplate.opsForZSet().incrementScore(key, new Ticket(2, 2), 2);
+		redisTemplate.opsForZSet().popMax(key);
+
+		System.out.println("==================================================");
+		System.out.println(redisTemplate.opsForZSet().range(key, 0, -1));
+		System.out.println("==================================================");
+
+	}
 }
