638cf843a8b3de17c3946109bf0a2bfed4af9abb
Kim HyungGeun <hyunggeun.kim@bugs.com>
2024-03-19 12:22:24 +0900

    [FIX] Redis 전용 테스트 레포지토리로 수정

--- spring-redis-test/src/main/java/com/example/springredistest/model/Ticket.java
+++ spring-redis-test/src/main/java/com/example/springredistest/model/Ticket.java
@@ -1,13 +1,13 @@
 package com.example.springredistest.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @Data
-@NoArgsConstructor
+@AllArgsConstructor
 public class Ticket {
-    private int id;
-    private int memberNo;
+	private int id;
+	private int memberNo;
 }
