3484dc80d37dee79708ae7ba9588d897a3ba3dc4
hgkim87 <hgkim8704@gmail.com>
2024-08-18 21:23:42 +0900

    [ADD] TreeSet 선언부 보기 위해 추가

---
+++ java-api-test/src/test/java/set/SetTest.java
@@ -0,0 +1,17 @@
+package list;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeSet;
+
+public class SetTest {
+
+    @Test
+    void treeSetTest() {
+        TreeSet treeSet = new TreeSet();
+    }
+
+}
