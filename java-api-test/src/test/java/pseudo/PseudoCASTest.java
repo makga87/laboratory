2f89530fb82475556bd9a6e45f704783f3c7b6d5
hgkim87 <makga87@naver.com>
2024-09-04 23:51:47 +0900

    [ADD] CAS 예제 테스트 코드 추가, 순수 자바코드만으론 원자적인 연산을 하는 CAS 구현이 안된다, atomic 패키지를 이용하던가 해야함

--- java-api-test/src/test/java/pseudo/PseudoCASTest.java
+++ java-api-test/src/test/java/pseudo/PseudoCASTest.java
@@ -2,14 +2,41 @@ package pseudo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class PseudoCASTest {
 
     private int count;
 
+    @RepeatedTest(5)
+    void casTest1() throws InterruptedException {
+        PseudoCAS cas = new PseudoCAS(0);
+        List<Thread> forWaiting = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            Thread t = new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    cas.incrementWithCAS();
+                }
+            });
+            forWaiting.add(t);
+            t.start();
+        }
+
+        // 모든 스레드가 완료될 때까지 기다립니다.
+        for (Thread t : forWaiting) {
+            t.join();
+        }
+
+
+        System.out.println("Final cas value: " + cas.getValue());
+    }
+
+
     @RepeatedTest(10)
-    void casTest() {
+    void casTest2() {
         PseudoCAS cas = new PseudoCAS(0);
 
         for (int i = 0; i < 100; i++) {
