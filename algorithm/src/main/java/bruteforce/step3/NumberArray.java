0ee5d22888f0234a477d70646a0ec5b82fd450b3
hgkim87 <hgkim8704@gmail.com>
2024-09-29 21:59:25 +0900

    [ADD] 누적합 1, 2, 3

---
+++ algorithm/src/main/java/bruteforce/step3/NumberArray.java
@@ -0,0 +1,25 @@
+package bruteforce.step3;
+
+public class NumberArray {
+
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 2;
+        int[] input = {3, -2, -4, -9, 0, 3, 7, 13, 8, -3};
+
+        int[] prefix = new int[A+1];
+        prefix[0] = 0;
+
+        for(int i = 1; i <= A; i++) {
+            prefix[i] = prefix[i-1] + input[i-1];
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for(int j = B; j < prefix.length; j++){
+            max = Math.max(max, prefix[j] - prefix[j-B]);
+        }
+
+        System.out.println(max);
+    }
+}
---
+++ algorithm/src/main/java/bruteforce/step3/Prefix.java
@@ -0,0 +1,26 @@
+package bruteforce.step3;
+
+public class Prefix {
+
+    public static void main(String[] args) {
+        int N = 10;
+        int[] data = new int[N];
+        int[] sum = new int[N+1];
+
+        for(int i = 1; i <= 10; i++){
+            data[i-1] = i;
+            sum[i] = data[i-1];
+            sum[i] = sum[i-1] + sum[i];
+        }
+
+        sum[0] = data[0];
+
+        for(int i = 1; i < 10; i++){
+            sum[i] = sum[i-1] + data[i];
+        }
+
+        for (int i : sum) {
+            System.out.println(i);
+        }
+    }
+}
