b6827f3be26eebe9d581045e29b3d2f835f3e5f5
hgkim87 <hgkim8704@gmail.com>
2024-10-16 21:43:29 +0900

    [ADD] leetcode 1405 exec1는 엣지케이스에서 막힘, exec2는 전부 통과

--- algorithm/src/test/java/leetcode/LongestHappyString_1405Test.java
+++ algorithm/src/test/java/leetcode/LongestHappyString_1405Test.java
@@ -9,54 +9,97 @@ import org.junit.jupiter.api.Test;
 
 class LongestHappyString_1405Test {
 
-	@Test
-	void test() {
+    @Test
+    void test() {
+        int a = 1;
+        int b = 1;
+        int c = 7;
+
 //		int a = 1;
-//		int b = 1;
+//		int b = 0;
 //		int c = 7;
 
-		int a = 1;
-		int b = 0;
-		int c = 7;
-
-		String result = exec1(a, b, c);
-		System.out.println(result);
-	}
+        String result = exec1(a, b, c);
+        System.out.println(result);
+    }
 
 
-	private String exec1(int a, int b, int c) {
+    private String exec1(int a, int b, int c) {
 
-		PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((v1, v2) -> v2.getValue().compareTo(v1.getValue()));
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((v1, v2) -> v2.getValue().compareTo(v1.getValue()));
 
-		Map<String, Integer> map = new HashMap<>();
-		map.put("a", a);
-		map.put("b", b);
-		map.put("c", c);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a", a);
+        map.put("b", b);
+        map.put("c", c);
 
-		pq.addAll(map.entrySet());
+        pq.addAll(map.entrySet());
 
-		StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-		while (pq.size() > 1) {
+        while (!pq.isEmpty()) {
 
-			Map.Entry<String, Integer> first = pq.poll();
-			Map.Entry<String, Integer> second = pq.poll();
+            Map.Entry<String, Integer> first = pq.poll();
+            Map.Entry<String, Integer> second = pq.poll();
 
-			if(first.getValue() >= 2) {
-				sb.append(first.getKey()).append(first.getKey());
-				first.setValue(first.getValue() - 2);
-				pq.offer(first);
-			}
+            if (first.getValue() >= 2) {
+                sb.append(first.getKey()).append(first.getKey());
+                first.setValue(first.getValue() - 2);
+                pq.offer(first);
+            }
 
-			if(second.getValue() >= 1) {
-				sb.append(second.getKey());
-				second.setValue(second.getValue() - 1);
-				pq.offer(second);
-			}
-		}
+            if (second == null || second.getValue() == 0) {
+                break;
+            }
 
-		return sb.toString();
+            if (second.getValue() >= 1) {
+                sb.append(second.getKey());
+                second.setValue(second.getValue() - 1);
+                pq.offer(second);
+            }
+        }
 
-	}
+        return sb.toString();
+
+    }
+
+    private String exec2(int a, int b, int c) {
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((v1, v2) -> v2.getValue().compareTo(v1.getValue()));
+
+        Map<Character, Integer> map = new HashMap<>();
+        if (a > 0) map.put('a', a);
+        if (b > 0) map.put('b', b);
+        if (c > 0) map.put('c', c);
+
+        pq.addAll(map.entrySet());
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!pq.isEmpty()) {
+            Map.Entry<Character, Integer> first = pq.poll();
+
+            if (sb.length() >= 2 &&
+                    sb.charAt(sb.length() - 1) == first.getKey() &&
+                    sb.charAt(sb.length() - 2) == first.getKey()) {
+
+                if (pq.isEmpty()) break;
+
+                Map.Entry<Character, Integer> second = pq.poll();
+                sb.append(second.getKey());
+                second.setValue(second.getValue() - 1);
+
+                if (second.getValue() > 0) pq.offer(second);
+                pq.offer(first);
+            } else {
+                sb.append(first.getKey());
+                first.setValue(first.getValue() - 1);
+
+                if (first.getValue() > 0) pq.offer(first);
+            }
+        }
+
+        return sb.toString();
+
+    }
 
 }
