93fb6363698eb7727b35cb1718976c5ef6f25584
Kim HyungGeun <makga87@naver.com>
2025-01-07 21:54:33 +0900

    [ADD] 런타임 상수풀 동적할당 테스트

--- java-api-test/src/test/java/string/StringConstantTest.java
+++ java-api-test/src/test/java/string/StringConstantTest.java
@@ -24,4 +24,16 @@ class StringConstantTest {
 		long end = System.currentTimeMillis();
 		System.out.println(end-start);
 	}
+
+
+	@Test
+	void 런타임상수풀_동적할당으로_참조주소가_같음을_확인(){
+		String str1 = new String("cat");  // 힙에 새로운 객체 생성
+		String str2 = new String("cat");  // 힙에 또 다른 새로운 객체 생성
+		System.out.println(str1 == str2);          // false: 다른 객체
+
+		String intern1 = str1.intern();  // 문자열 풀에 "cat" 추가 또는 찾기
+		String intern2 = str2.intern();  // 풀에서 동일한 "cat" 찾기
+		System.out.println(intern1 == intern2);     // true: 같은 풀의 객체 참조
+	}
 }
